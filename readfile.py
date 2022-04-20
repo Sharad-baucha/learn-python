6d43c236569b5c93158e0e221e9c9fa24e98f65b
Sharad-baucha <sharadshrestha@kbc.edu.np>
2022-04-20 11:58:24 +0545

    new commit

--- readfile.py
+++ readfile.py
@@ -1,10 +1,25 @@
-f = open("demofile.txt", "r")
-print(f.read())
-print("\n")
-f = open("demofile.txt", "a")
-f.write("\n Sharad noob mula!")
-f.close()
+# f = open("demofile.txt", "r")
+# print(f.read())
+# print("\n")
+# f = open("demofile.txt", "a")
+# f.write("\n Sharad noob mula!")
+# f.close()
 
-#open and read the file after the appending:
+# #open and read the file after the appending:
+# f = open("demofile.txt", "r")
+# print(f.read())
+
+# str1 = "hello world"
+# print(str1.replace("hello","noob"))
+
 f = open("demofile.txt", "r")
-print(f.read())
+str1 = f.read()
+print(str1)
+
+data = str1.replace("noob", "good")
+
+g = open("output.txt", "w")
+g.write(data)
+
+f.close()
+g.close()
