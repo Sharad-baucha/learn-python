0dabbff072bfc76a26237983ff52f0bf72824ee2
Sharad-baucha <sharadshrestha@kbc.edu.np>
2022-04-20 08:40:32 +0545

    new

---
+++ censored.py
@@ -0,0 +1,21 @@
+# import operator  # For contains function
+from better_profanity import profanity  # For censoring words
+
+file = open('swear.txt', 'r')
+user_input = input("Enter a text: ")
+swear_words = []
+swear_words.append(file.read())
+
+# Removing the escape sequences from the list
+
+swear_list = " ".join(swear_words)
+escapes = ''.join([chr(char) for char in range(1, 32)])
+translator = str.maketrans('', '', escapes)
+swear_words = swear_list.translate(translator)
+
+print(swear_words)
+
+censored = ""
+if str.__contains__(user_input, swear_words):
+    censored += user_input
+print(censored)
