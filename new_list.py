35406c1c19d2d94bfc21b7970c6332f8224b686f
Sharad-baucha <sharadshrestha@kbc.edu.np>
2022-04-22 12:11:26 +0545

    new commit 1

---
+++ new_list.py
@@ -0,0 +1,18 @@
+# thislist = [15 , 88 , 45 , 47 , 420 , 599]
+
+# for i in thislist:
+#     print(i)
+
+# keys = ['A','B','C']
+# values = [65,66,67]
+
+# thisdict = {}
+
+# for i in range(0 , len(keys)):
+#     thisdict[keys[i]] = values[i]
+    
+# #print(thisdict.items())
+# print(thisdict)
+
+dic1 = {}
+dic2 = {}
